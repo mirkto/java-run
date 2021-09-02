ac15c5853e3072c90dd53eb2907b063d70304184
mirkto <thehisev@mail.ru>
2021-09-02 21:52:28 +0300

    rework

--- src/PrimitivesVSBoxedBattle.java
+++ src/PrimitivesVSBoxedBattle.java
@@ -1,37 +1,40 @@
 public class PrimitivesVSBoxedBattle {
     public static void main(String[] args) {
-        long before, after, difPrimitives, difBoxed;
-
-        before = System.currentTimeMillis();
-        Primitives();
-        after = System.currentTimeMillis();
-        difPrimitives = after - before;
-
-        before = System.currentTimeMillis();
-        Boxed();
-        after = System.currentTimeMillis();
-        difBoxed = after - before;
-
-        System.out.println("Primitives: " + difPrimitives);
-        System.out.println("Boxed: " + difBoxed);
+        System.out.println("Primitives: " + differencePrimitives());
+        System.out.println("Boxed: " + differenceBoxed());
     }
 
-    private static void Primitives() {
-        int testArr[] = new int[10000000];
+    private static long differenceBoxed() {
+        long before, after;
+        before = System.currentTimeMillis();
+        boxed();
+        after = System.currentTimeMillis();
+        return after - before;
+    }
 
+    private static long differencePrimitives() {
+        long before, after;
+        before = System.currentTimeMillis();
+        primitives();
+        after = System.currentTimeMillis();
+        return after - before;
+    }
+
+    private static void primitives() {
+
+        int testArr1[] = new int[1000000];
         int i = 0;
-        for (int elem: testArr) {
-            elem = i * 100;
+        for (int elem: testArr1) {
+            elem = i + 100;
             ++i;
         }
     }
 
-    private static void Boxed() {
-        Integer testArr[] = new Integer[10000000];
-
-        int i = 0;
+    private static void boxed() {
+        Integer testArr[] = new Integer[1000000];
+        Integer i = 0;
         for (Integer elem: testArr) {
-            elem = i * 100;
+            elem = i + 100;
             ++i;
         }
     }
